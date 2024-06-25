7e28f4c02b4fb18b0b59a1515cfa8dc396957d33
hjc2000 <hjc.666@foxmail.com>
2024-06-25 16:36:20 +0800

    typedef struct AutoBitConverter

--- include/c-bsp-interface/AutoBitConverter.h
+++ include/c-bsp-interface/AutoBitConverter.h
@@ -2,6 +2,8 @@
 #include <c-bsp-interface/Endian.h>
 #include <stdint.h>
 
+/// @brief 在通信时需要在本机字节序和远程字节序之间转换。本结构体提供了一系列方法来进行这种操作。
+/// 设置好远程的字节序后，在转换时如果远程字节序与本机不一样，就会反转字节数组。
 typedef struct AutoBitConverter
 {
 	Endian _remote_endian;
