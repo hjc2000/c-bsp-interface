132b5b57b5b0c18c77dba8ef906619f2b2700c26
hjc2000 <hjc.666@foxmail.com>
2024-06-25 14:20:12 +0800

    const

--- include/c-bsp-interface/AutoBitConverter.c
+++ include/c-bsp-interface/AutoBitConverter.c
@@ -6,9 +6,9 @@ static uint8_t ShouldReverse(AutoBitConverter *this)
 	return LocalHostEndian() != this->_remote_endian;
 }
 
-uint16_t AutoBitConverter_ToUInt16(AutoBitConverter *this, uint8_t *buffer, int32_t offset)
+uint16_t AutoBitConverter_ToUInt16(AutoBitConverter *this, uint8_t const *buffer, int32_t offset)
 {
-	uint16_t ret = *(uint16_t *)(buffer + offset);
+	uint16_t ret = *(uint16_t const *)(buffer + offset);
 	if (ShouldReverse(this))
 	{
 		ReverseByteArray((uint8_t *)(&ret), 2);
@@ -17,9 +17,9 @@ uint16_t AutoBitConverter_ToUInt16(AutoBitConverter *this, uint8_t *buffer, int3
 	return ret;
 }
 
-int16_t AutoBitConverter_ToInt16(AutoBitConverter *this, uint8_t *buffer, int32_t offset)
+int16_t AutoBitConverter_ToInt16(AutoBitConverter *this, uint8_t const *buffer, int32_t offset)
 {
-	int16_t ret = *(int16_t *)(buffer + offset);
+	int16_t ret = *(int16_t const *)(buffer + offset);
 	if (ShouldReverse(this))
 	{
 		ReverseByteArray((uint8_t *)(&ret), 2);
@@ -28,9 +28,9 @@ int16_t AutoBitConverter_ToInt16(AutoBitConverter *this, uint8_t *buffer, int32_
 	return ret;
 }
 
-uint32_t AutoBitConverter_ToUInt32(AutoBitConverter *this, uint8_t *buffer, int32_t offset)
+uint32_t AutoBitConverter_ToUInt32(AutoBitConverter *this, uint8_t const *buffer, int32_t offset)
 {
-	uint32_t ret = *(uint32_t *)(buffer + offset);
+	uint32_t ret = *(uint32_t const *)(buffer + offset);
 	if (ShouldReverse(this))
 	{
 		ReverseByteArray((uint8_t *)(&ret), 4);
@@ -39,9 +39,9 @@ uint32_t AutoBitConverter_ToUInt32(AutoBitConverter *this, uint8_t *buffer, int3
 	return ret;
 }
 
-int32_t AutoBitConverter_ToInt32(AutoBitConverter *this, uint8_t *buffer, int32_t offset)
+int32_t AutoBitConverter_ToInt32(AutoBitConverter *this, uint8_t const *buffer, int32_t offset)
 {
-	int32_t ret = *(int32_t *)(buffer + offset);
+	int32_t ret = *(int32_t const *)(buffer + offset);
 	if (ShouldReverse(this))
 	{
 		ReverseByteArray((uint8_t *)(&ret), 4);
@@ -50,9 +50,9 @@ int32_t AutoBitConverter_ToInt32(AutoBitConverter *this, uint8_t *buffer, int32_
 	return ret;
 }
 
-uint64_t AutoBitConverter_ToUInt64(AutoBitConverter *this, uint8_t *buffer, int32_t offset)
+uint64_t AutoBitConverter_ToUInt64(AutoBitConverter *this, uint8_t const *buffer, int32_t offset)
 {
-	uint64_t ret = *(uint64_t *)(buffer + offset);
+	uint64_t ret = *(uint64_t const *)(buffer + offset);
 	if (ShouldReverse(this))
 	{
 		ReverseByteArray((uint8_t *)(&ret), 8);
@@ -61,9 +61,9 @@ uint64_t AutoBitConverter_ToUInt64(AutoBitConverter *this, uint8_t *buffer, int3
 	return ret;
 }
 
-int64_t AutoBitConverter_ToInt64(AutoBitConverter *this, uint8_t *buffer, int32_t offset)
+int64_t AutoBitConverter_ToInt64(AutoBitConverter *this, uint8_t const *buffer, int32_t offset)
 {
-	int64_t ret = *(int64_t *)(buffer + offset);
+	int64_t ret = *(int64_t const *)(buffer + offset);
 	if (ShouldReverse(this))
 	{
 		ReverseByteArray((uint8_t *)(&ret), 8);
@@ -72,9 +72,9 @@ int64_t AutoBitConverter_ToInt64(AutoBitConverter *this, uint8_t *buffer, int32_
 	return ret;
 }
 
-float AutoBitConverter_ToFloat(AutoBitConverter *this, uint8_t *buffer, int32_t offset)
+float AutoBitConverter_ToFloat(AutoBitConverter *this, uint8_t const *buffer, int32_t offset)
 {
-	float ret = *(float *)(buffer + offset);
+	float ret = *(float const *)(buffer + offset);
 	if (ShouldReverse(this))
 	{
 		ReverseByteArray((uint8_t *)(&ret), 4);
@@ -83,9 +83,9 @@ float AutoBitConverter_ToFloat(AutoBitConverter *this, uint8_t *buffer, int32_t 
 	return ret;
 }
 
-double AutoBitConverter_ToDouble(AutoBitConverter *this, uint8_t *buffer, int32_t offset)
+double AutoBitConverter_ToDouble(AutoBitConverter *this, uint8_t const *buffer, int32_t offset)
 {
-	double ret = *(double *)(buffer + offset);
+	double ret = *(double const *)(buffer + offset);
 	if (ShouldReverse(this))
 	{
 		ReverseByteArray((uint8_t *)(&ret), 8);
--- include/c-bsp-interface/AutoBitConverter.h
+++ include/c-bsp-interface/AutoBitConverter.h
@@ -7,14 +7,14 @@ typedef struct AutoBitConverter
 	Endian _remote_endian;
 } AutoBitConverter;
 
-uint16_t AutoBitConverter_ToUInt16(AutoBitConverter *this, uint8_t *buffer, int32_t offset);
-int16_t AutoBitConverter_ToInt16(AutoBitConverter *this, uint8_t *buffer, int32_t offset);
+uint16_t AutoBitConverter_ToUInt16(AutoBitConverter *this, uint8_t const *buffer, int32_t offset);
+int16_t AutoBitConverter_ToInt16(AutoBitConverter *this, uint8_t const *buffer, int32_t offset);
 
-uint32_t AutoBitConverter_ToUInt32(AutoBitConverter *this, uint8_t *buffer, int32_t offset);
-int32_t AutoBitConverter_ToInt32(AutoBitConverter *this, uint8_t *buffer, int32_t offset);
+uint32_t AutoBitConverter_ToUInt32(AutoBitConverter *this, uint8_t const *buffer, int32_t offset);
+int32_t AutoBitConverter_ToInt32(AutoBitConverter *this, uint8_t const *buffer, int32_t offset);
 
-uint64_t AutoBitConverter_ToUInt64(AutoBitConverter *this, uint8_t *buffer, int32_t offset);
-int64_t AutoBitConverter_ToInt64(AutoBitConverter *this, uint8_t *buffer, int32_t offset);
+uint64_t AutoBitConverter_ToUInt64(AutoBitConverter *this, uint8_t const *buffer, int32_t offset);
+int64_t AutoBitConverter_ToInt64(AutoBitConverter *this, uint8_t const *buffer, int32_t offset);
 
-float AutoBitConverter_ToFloat(AutoBitConverter *this, uint8_t *buffer, int32_t offset);
-double AutoBitConverter_ToDouble(AutoBitConverter *this, uint8_t *buffer, int32_t offset);
+float AutoBitConverter_ToFloat(AutoBitConverter *this, uint8_t const *buffer, int32_t offset);
+double AutoBitConverter_ToDouble(AutoBitConverter *this, uint8_t const *buffer, int32_t offset);
