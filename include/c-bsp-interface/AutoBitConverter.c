1fda6d8f638ddf36d2f99c9fd28045540c9daafb
hjc2000 <hjc.666@foxmail.com>
2024-06-25 13:58:48 +0800

    AutoBitConverter

--- include/c-bsp-interface/AutoBitConverter.c
+++ include/c-bsp-interface/AutoBitConverter.c
@@ -1,42 +1,95 @@
 #include "AutoBitConverter.h"
 #include <c-bsp-interface/Reverse.h>
 
-uint16_t AutoBitConverter_ToUInt16(AutoBitConverter *this, uint8_t *buffer, int offset)
+static uint8_t ShouldReverse(AutoBitConverter *this)
 {
-	return 0;
+	return LocalHostEndian() != this->_remote_endian;
 }
 
-int16_t AutoBitConverter_ToInt16(AutoBitConverter *this, uint8_t *buffer, int offset)
+uint16_t AutoBitConverter_ToUInt16(AutoBitConverter *this, uint8_t *buffer, int32_t offset)
 {
-	return 0;
+	uint16_t ret = *(uint16_t *)(buffer + offset);
+	if (ShouldReverse(this))
+	{
+		ReverseByteArray((uint8_t *)(&ret), 2);
+	}
+
+	return ret;
 }
 
-uint32_t AutoBitConverter_ToUInt32(AutoBitConverter *this, uint8_t *buffer, int offset)
+int16_t AutoBitConverter_ToInt16(AutoBitConverter *this, uint8_t *buffer, int32_t offset)
 {
-	return 0;
+	int16_t ret = *(int16_t *)(buffer + offset);
+	if (ShouldReverse(this))
+	{
+		ReverseByteArray((uint8_t *)(&ret), 2);
+	}
+
+	return ret;
 }
 
-int32_t AutoBitConverter_ToInt32(AutoBitConverter *this, uint8_t *buffer, int offset)
+uint32_t AutoBitConverter_ToUInt32(AutoBitConverter *this, uint8_t *buffer, int32_t offset)
 {
-	return 0;
+	uint32_t ret = *(uint32_t *)(buffer + offset);
+	if (ShouldReverse(this))
+	{
+		ReverseByteArray((uint8_t *)(&ret), 4);
+	}
+
+	return ret;
 }
 
-uint64_t AutoBitConverter_ToUInt64(AutoBitConverter *this, uint8_t *buffer, int offset)
+int32_t AutoBitConverter_ToInt32(AutoBitConverter *this, uint8_t *buffer, int32_t offset)
 {
-	return 0;
+	int32_t ret = *(int32_t *)(buffer + offset);
+	if (ShouldReverse(this))
+	{
+		ReverseByteArray((uint8_t *)(&ret), 4);
+	}
+
+	return ret;
 }
 
-int64_t AutoBitConverter_ToInt64(AutoBitConverter *this, uint8_t *buffer, int offset)
+uint64_t AutoBitConverter_ToUInt64(AutoBitConverter *this, uint8_t *buffer, int32_t offset)
 {
-	return 0;
+	uint64_t ret = *(uint64_t *)(buffer + offset);
+	if (ShouldReverse(this))
+	{
+		ReverseByteArray((uint8_t *)(&ret), 8);
+	}
+
+	return ret;
 }
 
-float AutoBitConverter_ToFloat(AutoBitConverter *this, uint8_t *buffer, int offset)
+int64_t AutoBitConverter_ToInt64(AutoBitConverter *this, uint8_t *buffer, int32_t offset)
 {
-	return 0.0f;
+	int64_t ret = *(int64_t *)(buffer + offset);
+	if (ShouldReverse(this))
+	{
+		ReverseByteArray((uint8_t *)(&ret), 8);
+	}
+
+	return ret;
 }
 
-double AutoBitConverter_ToDouble(AutoBitConverter *this, uint8_t *buffer, int offset)
+float AutoBitConverter_ToFloat(AutoBitConverter *this, uint8_t *buffer, int32_t offset)
 {
-	return 0.0;
+	float ret = *(float *)(buffer + offset);
+	if (ShouldReverse(this))
+	{
+		ReverseByteArray((uint8_t *)(&ret), 4);
+	}
+
+	return ret;
+}
+
+double AutoBitConverter_ToDouble(AutoBitConverter *this, uint8_t *buffer, int32_t offset)
+{
+	double ret = *(double *)(buffer + offset);
+	if (ShouldReverse(this))
+	{
+		ReverseByteArray((uint8_t *)(&ret), 8);
+	}
+
+	return ret;
 }
--- include/c-bsp-interface/AutoBitConverter.h
+++ include/c-bsp-interface/AutoBitConverter.h
@@ -7,14 +7,14 @@ typedef struct AutoBitConverter
 	Endian _remote_endian;
 } AutoBitConverter;
 
-uint16_t AutoBitConverter_ToUInt16(AutoBitConverter *this, uint8_t *buffer, int offset);
-int16_t AutoBitConverter_ToInt16(AutoBitConverter *this, uint8_t *buffer, int offset);
+uint16_t AutoBitConverter_ToUInt16(AutoBitConverter *this, uint8_t *buffer, int32_t offset);
+int16_t AutoBitConverter_ToInt16(AutoBitConverter *this, uint8_t *buffer, int32_t offset);
 
-uint32_t AutoBitConverter_ToUInt32(AutoBitConverter *this, uint8_t *buffer, int offset);
-int32_t AutoBitConverter_ToInt32(AutoBitConverter *this, uint8_t *buffer, int offset);
+uint32_t AutoBitConverter_ToUInt32(AutoBitConverter *this, uint8_t *buffer, int32_t offset);
+int32_t AutoBitConverter_ToInt32(AutoBitConverter *this, uint8_t *buffer, int32_t offset);
 
-uint64_t AutoBitConverter_ToUInt64(AutoBitConverter *this, uint8_t *buffer, int offset);
-int64_t AutoBitConverter_ToInt64(AutoBitConverter *this, uint8_t *buffer, int offset);
+uint64_t AutoBitConverter_ToUInt64(AutoBitConverter *this, uint8_t *buffer, int32_t offset);
+int64_t AutoBitConverter_ToInt64(AutoBitConverter *this, uint8_t *buffer, int32_t offset);
 
-float AutoBitConverter_ToFloat(AutoBitConverter *this, uint8_t *buffer, int offset);
-double AutoBitConverter_ToDouble(AutoBitConverter *this, uint8_t *buffer, int offset);
+float AutoBitConverter_ToFloat(AutoBitConverter *this, uint8_t *buffer, int32_t offset);
+double AutoBitConverter_ToDouble(AutoBitConverter *this, uint8_t *buffer, int32_t offset);
