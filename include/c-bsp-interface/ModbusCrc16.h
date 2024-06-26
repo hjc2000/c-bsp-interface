3eaafd2d79b6ccac7bf126c873939e2a0b61599a
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-06-26 22:57:35 +0800

    crc

--- include/c-bsp-interface/ModbusCrc16.h
+++ include/c-bsp-interface/ModbusCrc16.h
@@ -3,10 +3,24 @@
 
 typedef struct ModbusCrc16
 {
+	/// @brief CRC16 寄存器。用户不要手动修改
 	uint16_t _crc16_register;
+
+	/// @brief 生成数。用户不要手动修改。
 	uint16_t _polynomial;
 } ModbusCrc16;
 
+/// @brief 初始化。会使用默认的生成多项式。
+/// @param
 void ModbusCrc16_Init(ModbusCrc16 *this);
+
+/// @brief 添加一个数进行计算。
+/// @param
+/// @param data
 void ModbusCrc16_Add(ModbusCrc16 *this, uint8_t data);
+
+/// @brief 添加一个数组中的数进行计算。会逐字节添加，进行计算。
+/// @param
+/// @param datas
+/// @param count
 void ModbusCrc16_AddArray(ModbusCrc16 *this, uint8_t *datas, uint32_t count);
