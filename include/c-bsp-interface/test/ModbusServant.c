e189f2086c52602724f54acf8c749c78c1969398
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-06-27 00:03:29 +0800

    typedef struct ModbusServant

--- include/c-bsp-interface/test/ModbusServant.c
+++ include/c-bsp-interface/test/ModbusServant.c
@@ -1,47 +1,9 @@
 #include "ModbusServant.h"
-#include <c-bsp-interface/KeyScanner.h>
+#include <c-bsp-interface/ModbusCrc16.h>
 
 static ModbusServant *_servant;
-static KeyScanner _key_scanner;
-
-static void InitKeyScanner()
-{
-#pragma region 初始化 4 个 Bitset
-	{
-		static uint8_t set[2];
-		_key_scanner._last_key_pressed_state_bitset._set = set;
-		_key_scanner._last_key_pressed_state_bitset._size = 2;
-	}
-	{
-		static uint8_t set[2];
-		_key_scanner._key_pressed_state_bitset._set = set;
-		_key_scanner._key_pressed_state_bitset._size = 2;
-	}
-	{
-		static uint8_t set[2];
-		_key_scanner._key_down_event_bitset._set = set;
-		_key_scanner._key_down_event_bitset._size = 2;
-	}
-	{
-		static uint8_t set[2];
-		_key_scanner._key_up_event_bitset._set = set;
-		_key_scanner._key_up_event_bitset._size = 2;
-	}
-#pragma endregion
-
-	{
-		static IKey *keys[2];
-		keys[0] = _servant->_key0;
-		keys[1] = _servant->_key1;
-		_key_scanner._keys = keys;
-	}
-
-	_key_scanner._key_count = 2;
-	_key_scanner.DelayMilliseconds = _servant->DelayMilliseconds;
-}
 
 void ModbusServant_Start(ModbusServant *this)
 {
 	_servant = this;
-	InitKeyScanner();
 }
--- include/c-bsp-interface/test/ModbusServant.h
+++ include/c-bsp-interface/test/ModbusServant.h
@@ -2,18 +2,14 @@
 #include <c-bsp-interface/AsyncStream.h>
 #include <c-bsp-interface/IDigitalLed.h>
 #include <c-bsp-interface/IKey.h>
-#include <c-bsp-interface/ModbusCrc16.h>
+#include <c-bsp-interface/KeyScanner.h>
 
 typedef struct ModbusServant
 {
 	AsyncStream *_stream;
 	IDigitalLed *_red_led;
 	IDigitalLed *_green_led;
-	IKey *_key0;
-	IKey *_key1;
-
-	/// @brief 延时函数。用来消抖。
-	void (*DelayMilliseconds)(uint32_t ms);
+	KeyScanner *_key_scanner;
 } ModbusServant;
 
 /// @brief 单例模式，只能启动一个实例。
