71eede983ef802c21f39fec8fb194025623c8654
hjc2000 <hjc.666@foxmail.com>
2024-06-27 11:51:16 +0800

    typedef struct ModbusServant

--- include/c-bsp-interface/test/ModbusServant.h
+++ include/c-bsp-interface/test/ModbusServant.h
@@ -2,14 +2,14 @@
 #include <c-bsp-interface/AsyncStream.h>
 #include <c-bsp-interface/IDigitalLed.h>
 #include <c-bsp-interface/IKey.h>
-#include <c-bsp-interface/KeyScanner.h>
 
 typedef struct ModbusServant
 {
 	AsyncStream *_stream;
 	IDigitalLed *_red_led;
 	IDigitalLed *_green_led;
-	KeyScanner *_key_scanner;
+	IKey *_key0;
+	IKey *_key1;
 } ModbusServant;
 
 void ModbusServant_Start(ModbusServant *this);
