339f1188e34ae5769dd42aa7f896477b51d9fb3e
hjc2000 <hjc.666@foxmail.com>
2024-06-28 09:30:03 +0800

    HandlePdu

--- include/c-bsp-interface/test/ModbusServant.c
+++ include/c-bsp-interface/test/ModbusServant.c
@@ -1,22 +1,22 @@
 #include "ModbusServant.h"
 
-/// @brief 处理广播帧
+/// @brief 处理广播的 PDU
 /// @note 被调用说明 CRC 校验通过了。
 ///
 /// @param
-/// @param pdu_frame 包含功能码和数据，不包含站号和 CRC16
+/// @param pdu 包含功能码和数据，不包含站号和 CRC16
 /// @param count 字节数
-static void HandleBrocastFrame(ModbusServant *this, uint8_t *pdu_frame, int32_t count)
+static void HandleBrocastPdu(ModbusServant *this, uint8_t *pdu, int32_t count)
 {
 }
 
-/// @brief 处理普通帧
+/// @brief 处理普通的 PDU
 /// @note 被调用说明 CRC 校验通过了。
 ///
 /// @param
-/// @param pdu_frame 包含功能码和数据，不包含站号和 CRC16
+/// @param pdu 包含功能码和数据，不包含站号和 CRC16
 /// @param count 字节数
-static void HandleFrame(ModbusServant *this, uint8_t *pdu_frame, int32_t count)
+static void HandlePdu(ModbusServant *this, uint8_t *pdu, int32_t count)
 {
 }
 
@@ -33,18 +33,16 @@ void ModbusServant_Init(ModbusServant *this,
 void ModbusServant_SendBuffer(ModbusServant *this,
 							  uint8_t *buffer, int32_t offset, int32_t count)
 {
-	if (count <= 2)
+	if (count <= 3)
 	{
-		/* 因为 CRC16 占 2 字节，所以帧大小不可能小于等于 2 字节，如果出现这种情况，
-		 * 一定是帧出错了。
-		 *
-		 * 此时连帧都不完整，也不知道是发给谁的，所以就不发回例外响应了。
+		/* ADU 比 PDU 多出来 3 个字节。其中 CRC16 占用 2 个字节，站号占用 1 个字节。
+		 * 如果整个 ADU 的大小小于等于 3，说明 PDU 为空，这种帧没有意义。
 		 */
 		return;
 	}
 
-	uint8_t *frame = buffer + offset;
-	if (frame[0] != this->_servant_address && frame[0] != 0)
+	uint8_t *adu = buffer + offset;
+	if (adu[0] != this->_servant_address && adu[0] != 0)
 	{
 		// 站号与本站号不匹配，且不等于 0，即不是广播帧。
 		return;
@@ -53,9 +51,9 @@ void ModbusServant_SendBuffer(ModbusServant *this,
 	// 站号匹配或者是广播帧
 	// 进行 CRC 校验
 	ModbusCrc16_ResetRegister(&this->_crc);
-	ModbusCrc16_AddArray(&this->_crc, frame, count - 2);
+	ModbusCrc16_AddArray(&this->_crc, adu, count - 2);
 	uint8_t crc16_check_result = ModbusCrc16_CompareRegister(&this->_crc,
-															 frame + count - 2,
+															 adu + count - 2,
 															 this->_crc16_endian);
 	if (!crc16_check_result)
 	{
@@ -64,14 +62,15 @@ void ModbusServant_SendBuffer(ModbusServant *this,
 	}
 
 	// CRC 校验通过
-	if (frame[0] == 0)
+	uint8_t *pdu = adu + 1;
+	if (adu[0] == 0)
 	{
 		// 广播帧
-		HandleBrocastFrame(this, frame, count - 2);
+		HandleBrocastPdu(this, pdu, count - 2);
 		return;
 	}
 
 	// 普通帧
-	HandleFrame(this, frame, count - 2);
+	HandlePdu(this, pdu, count - 2);
 }
 #pragma endregion
