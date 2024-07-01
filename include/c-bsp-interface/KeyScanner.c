3dfb2e25af41d3d0ddeb20d24d37ad47b5caf141
hjc2000 <hjc.666@foxmail.com>
2024-07-01 16:47:40 +0800

    KeyScanner_StackHeapAlloc

--- include/c-bsp-interface/KeyScanner.c
+++ include/c-bsp-interface/KeyScanner.c
@@ -1,11 +1,12 @@
 #include "KeyScanner.h"
+#include <c-bsp-interface/memory/StackHeap.h>
 
 static void GetCurrentKeyPressedState(KeyScanner *this)
 {
 	// 遍历每个按键
 	for (uint32_t i = 0; i < this->_key_count; i++)
 	{
-		Bitset *bitset = &this->_key_pressed_state_bitset;
+		Bitset *bitset = this->_key_pressed_state_bitset;
 		IKey *key = this->_keys[i];
 		Bitset_SetBitValue(bitset, i, key->KeyIsDown());
 	}
@@ -15,47 +16,70 @@ static void GetCurrentKeyPressedState(KeyScanner *this)
 	for (uint32_t i = 0; i < this->_key_count; i++)
 	{
 		uint8_t value = this->_keys[i]->KeyIsDown() &&
-						Bitset_GetBit(&this->_key_pressed_state_bitset, i);
+						Bitset_GetBit(this->_key_pressed_state_bitset, i);
 
-		Bitset_SetBitValue(&this->_key_pressed_state_bitset, i, value);
+		Bitset_SetBitValue(this->_key_pressed_state_bitset, i, value);
 	}
+}
+
+KeyScanner *KeyScanner_StackHeapAlloc(int32_t key_count,
+									  void (*delay_milliseconds)(uint32_t ms))
+{
+	if (key_count <= 0)
+	{
+		return 0;
+	}
+
+	KeyScanner *this = (KeyScanner *)StackHeapAlignAlloc(sizeof(KeyScanner), sizeof(KeyScanner *));
+
+	// 能够装下 key_count 个指针的指针数组
+	this->_keys = StackHeapAlignAlloc(sizeof(IKey *) * key_count, 4);
+	this->_key_count = key_count;
+
+	this->_key_pressed_state_bitset = Bitset_StackHeapAlloc(key_count);
+	this->_key_down_event_bitset = Bitset_StackHeapAlloc(key_count);
+	this->_key_up_event_bitset = Bitset_StackHeapAlloc(key_count);
+	this->_last_key_pressed_state_bitset = Bitset_StackHeapAlloc(key_count);
+
+	this->DelayMilliseconds = delay_milliseconds;
+	return this;
 }
 
 void KeyScanner_Scan(KeyScanner *this)
 {
 	GetCurrentKeyPressedState(this);
 
-	Bitset_CopyFrom(&this->_key_down_event_bitset,
-					&this->_last_key_pressed_state_bitset);
+	Bitset_CopyFrom(this->_key_down_event_bitset,
+					this->_last_key_pressed_state_bitset);
 
-	Bitset_Not(&this->_key_down_event_bitset);
+	Bitset_Not(this->_key_down_event_bitset);
 
-	Bitset_And(&this->_key_down_event_bitset,
-			   &this->_key_pressed_state_bitset);
+	Bitset_And(this->_key_down_event_bitset,
+			   this->_key_pressed_state_bitset);
 
-	Bitset_CopyFrom(&this->_key_up_event_bitset,
-					&this->_key_pressed_state_bitset);
+	Bitset_CopyFrom(this->_key_up_event_bitset,
+					this->_key_pressed_state_bitset);
 
-	Bitset_Not(&this->_key_up_event_bitset);
+	Bitset_Not(this->_key_up_event_bitset);
 
-	Bitset_And(&this->_key_up_event_bitset,
-			   &this->_last_key_pressed_state_bitset);
+	Bitset_And(this->_key_up_event_bitset,
+			   this->_last_key_pressed_state_bitset);
 
-	Bitset_CopyFrom(&this->_last_key_pressed_state_bitset,
-					&this->_key_pressed_state_bitset);
+	Bitset_CopyFrom(this->_last_key_pressed_state_bitset,
+					this->_key_pressed_state_bitset);
 }
 
 uint8_t KeyScanner_KeyIsPressed(KeyScanner *this, uint32_t key_index)
 {
-	return Bitset_GetBit(&this->_key_pressed_state_bitset, key_index);
+	return Bitset_GetBit(this->_key_pressed_state_bitset, key_index);
 }
 
 uint8_t KeyScanner_HasKeyDownEvent(KeyScanner *this, uint32_t key_index)
 {
-	return Bitset_GetBit(&this->_key_down_event_bitset, key_index);
+	return Bitset_GetBit(this->_key_down_event_bitset, key_index);
 }
 
 uint8_t KeyScanner_HasKeyUpEvent(KeyScanner *this, uint32_t key_index)
 {
-	return Bitset_GetBit(&this->_key_up_event_bitset, key_index);
+	return Bitset_GetBit(this->_key_up_event_bitset, key_index);
 }
--- include/c-bsp-interface/KeyScanner.h
+++ include/c-bsp-interface/KeyScanner.h
@@ -5,10 +5,10 @@
 
 typedef struct KeyScanner
 {
-	Bitset _last_key_pressed_state_bitset;
-	Bitset _key_pressed_state_bitset;
-	Bitset _key_down_event_bitset;
-	Bitset _key_up_event_bitset;
+	Bitset *_last_key_pressed_state_bitset;
+	Bitset *_key_pressed_state_bitset;
+	Bitset *_key_down_event_bitset;
+	Bitset *_key_up_event_bitset;
 
 	/// @brief 将按键实例的指针放到数组中，赋值给本字段
 	IKey **_keys;
@@ -19,6 +19,9 @@ typedef struct KeyScanner
 	/// @brief 延时函数。用来消抖。
 	void (*DelayMilliseconds)(uint32_t ms);
 } KeyScanner;
+
+KeyScanner *KeyScanner_StackHeapAlloc(int32_t key_count,
+									  void (*delay_milliseconds)(uint32_t ms));
 
 /// @brief 执行扫描
 /// @param
