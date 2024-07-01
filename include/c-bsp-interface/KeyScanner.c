23ac90d2219ea268ac2bcf4f0afd750212ba979d
hjc2000 <hjc.666@foxmail.com>
2024-07-01 16:58:15 +0800

    typedef struct KeyScanner KeyScanner;

--- include/c-bsp-interface/KeyScanner.c
+++ include/c-bsp-interface/KeyScanner.c
@@ -1,5 +1,23 @@
 #include "KeyScanner.h"
+#include <c-bsp-interface/Bitset.h>
 #include <c-bsp-interface/memory/StackHeap.h>
+
+typedef struct KeyScanner
+{
+	Bitset *_last_key_pressed_state_bitset;
+	Bitset *_key_pressed_state_bitset;
+	Bitset *_key_down_event_bitset;
+	Bitset *_key_up_event_bitset;
+
+	/// @brief 将按键实例的指针放到数组中，赋值给本字段
+	IKey **_keys;
+
+	/// @brief 按键个数
+	int32_t _key_count;
+
+	/// @brief 延时函数。用来消抖。
+	void (*DelayMilliseconds)(uint32_t ms);
+} KeyScanner;
 
 static void GetCurrentKeyPressedState(KeyScanner *this)
 {
--- include/c-bsp-interface/KeyScanner.h
+++ include/c-bsp-interface/KeyScanner.h
@@ -1,24 +1,8 @@
 #pragma once
-#include <c-bsp-interface/Bitset.h>
 #include <c-bsp-interface/IKey.h>
 #include <stdint.h>
 
-typedef struct KeyScanner
-{
-	Bitset *_last_key_pressed_state_bitset;
-	Bitset *_key_pressed_state_bitset;
-	Bitset *_key_down_event_bitset;
-	Bitset *_key_up_event_bitset;
-
-	/// @brief 将按键实例的指针放到数组中，赋值给本字段
-	IKey **_keys;
-
-	/// @brief 按键个数
-	int32_t _key_count;
-
-	/// @brief 延时函数。用来消抖。
-	void (*DelayMilliseconds)(uint32_t ms);
-} KeyScanner;
+typedef struct KeyScanner KeyScanner;
 
 KeyScanner *KeyScanner_StackHeapAlloc(int32_t key_count,
 									  void (*delay_milliseconds)(uint32_t ms));
