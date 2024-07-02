b8eb3af5d17bf0999b0f30a47a8e5e7c2bc5aa7b
hjc2000 <hjc.666@foxmail.com>
2024-07-02 16:26:58 +0800

    ModbusBitConverter_GetBytesFromUInt32

--- include/c-bsp-interface/modbus/ModbusBitConverter.c
+++ include/c-bsp-interface/modbus/ModbusBitConverter.c
@@ -1,6 +1,7 @@
 #include "ModbusBitConverter.h"
 #include <c-bsp-interface/Endian.h>
 #include <c-bsp-interface/Reverse.h>
+#include <string.h>
 
 #pragma region 转数字
 uint16_t ModbusBitConverter_ToUInt16(ModbusBitConverterUnit unit,
@@ -220,6 +221,54 @@ double ModbusBitConverter_ToDouble(ModbusBitConverterUnit unit,
 void ModbusBitConverter_GetBytesFromUInt16(ModbusBitConverterUnit unit,
 										   uint16_t value, uint8_t *out_buffer, int32_t offset)
 {
+	uint8_t *value_buffer = (uint8_t *)(&value);
+	memcpy(out_buffer + offset, value_buffer, 2);
+	if (LocalHostEndian() == LittleEndian)
+	{
+		ReverseByteArray(out_buffer + offset, 2);
+	}
+}
+
+void ModbusBitConverter_GetBytesFromInt16(ModbusBitConverterUnit unit,
+										  int16_t value, uint8_t *out_buffer, int32_t offset)
+{
+	uint8_t *value_buffer = (uint8_t *)(&value);
+	memcpy(out_buffer + offset, value_buffer, 2);
+	if (LocalHostEndian() == LittleEndian)
+	{
+		ReverseByteArray(out_buffer + offset, 2);
+	}
+}
+
+void ModbusBitConverter_GetBytesFromUInt32(ModbusBitConverterUnit unit,
+										   uint32_t value, uint8_t *out_buffer, int32_t offset)
+{
+	uint8_t *value_buffer = (uint8_t *)(&value);
+	memcpy(out_buffer + offset, value_buffer, 4);
+	if (unit == ModbusBitConverterUnit_Whole)
+	{
+		if (LocalHostEndian() == LittleEndian)
+		{
+			// 本机是小端序，翻转后返回
+			ReverseByteArray(out_buffer + offset, 4);
+			return;
+		}
+
+		// 本机是大端序，不用翻转，直接返回
+		return;
+	}
+
+	// 逐个记录进行转换
+	if (LocalHostEndian() == BigEndian)
+	{
+		/* 本机是大端序，因为低 16 位要在第 1 个记录，所以先整体翻转，随后再逐个记录再次翻转，
+		 * 使每个记录变成大端序
+		 */
+		ReverseByteArray(out_buffer + offset, 4);
+	}
+
+	// 逐个记录进行翻转，也就是逐 2 字节进行翻转
+	ReverseByteArrayPerElement(out_buffer + offset, 2, 2);
 }
 
 #pragma endregion
