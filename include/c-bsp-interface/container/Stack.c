1e54982f7a0ff39219e74b807ff88ea32b0e0cd3
hjc2000 <hjc.666@foxmail.com>
2024-07-03 10:49:46 +0800

    Stack

--- include/c-bsp-interface/container/Stack.c
+++ include/c-bsp-interface/container/Stack.c
@@ -1,21 +1,43 @@
 #include "Stack.h"
+#include <c-bsp-interface/memory/StackHeap.h>
+#include <string.h>
 
 typedef struct Stack
 {
-	/* data */
+	uint8_t *_buffer;
+	int32_t _max_size;
+	int32_t _sp;
 } Stack;
 
 Stack *Stack_StackHeapAlloc(int32_t max_size)
 {
+	Stack *this = StackHeapAlignAlloc(sizeof(Stack), 4);
+	this->_buffer = StackHeapAlloc(max_size);
+	this->_max_size = max_size;
+	this->_sp = 0;
 	return 0;
 }
 
-uint8_t Stack_Push(void *data, int32_t size)
+uint8_t Stack_Push(Stack *this, void *data, int32_t size)
 {
-	return 0;
+	if (this->_sp + size > this->_max_size)
+	{
+		return 0;
+	}
+
+	memcpy(this->_buffer + this->_sp, data, size);
+	this->_sp += size;
+	return 1;
 }
 
-uint8_t Stack_Pop(void *out, int32_t size)
+uint8_t Stack_Pop(Stack *this, void *out, int32_t size)
 {
-	return 0;
+	if (this->_sp - size < 0)
+	{
+		return 0;
+	}
+
+	this->_sp -= size;
+	memcpy(out, this->_buffer + this->_sp, size);
+	return 1;
 }
--- include/c-bsp-interface/container/Stack.h
+++ include/c-bsp-interface/container/Stack.h
@@ -3,16 +3,19 @@
 
 typedef struct Stack Stack;
 
+/// @brief 在栈堆上构造 Stack。
+/// @param max_size 栈的最大大小。
+/// @return
 Stack *Stack_StackHeapAlloc(int32_t max_size);
 
 /// @brief 入栈指定大小的数据
 /// @param data 要入栈的数据的指针。
 /// @param size 要入栈的字节数。
 /// @return 成功返回 true，失败返回 false。
-uint8_t Stack_Push(void *data, int32_t size);
+uint8_t Stack_Push(Stack *this, void *data, int32_t size);
 
 /// @brief 出栈指定大小的数据。
 /// @param out 出栈的数据会被复制到该指针指向的内存。
 /// @param size 要出栈的字节数。
 /// @return 成功返回 true，失败返回 false。
-uint8_t Stack_Pop(void *out, int32_t size);
+uint8_t Stack_Pop(Stack *this, void *out, int32_t size);
