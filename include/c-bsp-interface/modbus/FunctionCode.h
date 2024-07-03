35c9669c419c40f96fc5b2735fbf165a50eda4af
hjc2000 <hjc.666@foxmail.com>
2024-07-03 13:49:15 +0800

    ModbusFunctionCode_

--- include/c-bsp-interface/modbus/FunctionCode.h
+++ include/c-bsp-interface/modbus/FunctionCode.h
@@ -1,29 +1,29 @@
 #pragma once
 
 /// @brief 功能码枚举。
-typedef enum FunctionCode
+typedef enum ModbusFunctionCode
 {
 	/// @brief 读取一组线圈
-	ReadCoils = 0x01,
+	ModbusFunctionCode_ReadCoils = 0x01,
 
 	/// @brief 读取一组输入位
-	ReadInputBits = 0x02,
+	ModbusFunctionCode_ReadInputBits = 0x02,
 
 	/// @brief 读取一组保持寄存器
-	ReadHoldingRegisters = 0x03,
+	ModbusFunctionCode_ReadHoldingRegisters = 0x03,
 
 	/// @brief 读取一组输入寄存器
-	ReadInputRegisters = 0x04,
+	ModbusFunctionCode_ReadInputRegisters = 0x04,
 
 	/// @brief 写单个线圈
-	WriteSingleCoil = 0x05,
+	ModbusFunctionCode_WriteSingleCoil = 0x05,
 
 	/// @brief 写入一组线圈
-	WriteCoils = 0x0f,
+	ModbusFunctionCode_WriteCoils = 0x0f,
 
 	/// @brief 写入多个保持寄存器
-	WriteHoldingRegisters = 0x10,
+	ModbusFunctionCode_WriteHoldingRegisters = 0x10,
 
 	/// @brief 诊断
-	Diagnosis = 0x08,
-} FunctionCode;
+	ModbusFunctionCode_Diagnosis = 0x08,
+} ModbusFunctionCode;
--- include/c-bsp-interface/modbus/ModbusServant.c
+++ include/c-bsp-interface/modbus/ModbusServant.c
@@ -1,13 +1,18 @@
 #include "ModbusServant.h"
 #include <c-bsp-interface/memory/StackHeap.h>
+#include <c-bsp-interface/modbus/FunctionCode.h>
+
+static void HandleReadCoils(ModbusServant *o, uint8_t *pdu, int32_t pdu_size)
+{
+}
 
 /// @brief 处理广播的 PDU
 /// @note 被调用说明 CRC 校验通过了。
 ///
 /// @param
 /// @param pdu modbus PDU
-/// @param count pdu 的字节数
-static void HandleBrocastPdu(ModbusServant *o, uint8_t *pdu, int32_t count)
+/// @param pdu_size pdu 的字节数
+static void HandleBrocastPdu(ModbusServant *o, uint8_t *pdu, int32_t pdu_size)
 {
 }
 
@@ -16,8 +21,8 @@ static void HandleBrocastPdu(ModbusServant *o, uint8_t *pdu, int32_t count)
 ///
 /// @param
 /// @param pdu modbus PDU
-/// @param count pdu 的字节数
-static void HandlePdu(ModbusServant *o, uint8_t *pdu, int32_t count)
+/// @param pdu_size pdu 的字节数
+static void HandlePdu(ModbusServant *o, uint8_t *pdu, int32_t pdu_size)
 {
 }
 
@@ -36,9 +41,9 @@ void ModbusServant_Init(ModbusServant *o,
 }
 
 void ModbusServant_ParseReceivedBuffer(ModbusServant *o,
-									   uint8_t *buffer, int32_t offset, int32_t count)
+									   uint8_t *buffer, int32_t offset, int32_t size)
 {
-	if (count <= 3)
+	if (size <= 3)
 	{
 		/* ADU 比 PDU 多出来 3 个字节。其中 CRC16 占用 2 个字节，站号占用 1 个字节。
 		 * 如果整个 ADU 的大小小于等于 3，说明 PDU 为空，这种帧没有意义。
@@ -56,9 +61,9 @@ void ModbusServant_ParseReceivedBuffer(ModbusServant *o,
 	// 站号匹配或者是广播帧
 	// 进行 CRC 校验
 	ModbusCrc16_ResetRegister(&o->_crc);
-	ModbusCrc16_AddArray(&o->_crc, adu, count - 2);
+	ModbusCrc16_AddArray(&o->_crc, adu, size - 2);
 	uint8_t crc16_check_result = ModbusCrc16_CompareRegister(&o->_crc,
-															 adu + count - 2,
+															 adu + size - 2,
 															 o->_crc16_endian);
 	if (!crc16_check_result)
 	{
@@ -67,14 +72,15 @@ void ModbusServant_ParseReceivedBuffer(ModbusServant *o,
 	}
 
 	// CRC 校验通过
+	// PDU 包括功能码和信息域。是 ADU 去掉头部 1 个字节的站号和尾部 2 个字节的 CRC16 后的结果。
 	uint8_t *pdu = adu + 1;
 	if (adu[0] == 0)
 	{
 		// 广播帧
-		HandleBrocastPdu(o, pdu, count - 2);
+		HandleBrocastPdu(o, pdu, size - 3);
 		return;
 	}
 
 	// 普通帧
-	HandlePdu(o, pdu, count - 2);
+	HandlePdu(o, pdu, size - 3);
 }
