171e82ac294a1bc0ae9f8177a7d07cdc1bb3a481
hjc2000 <hjc.666@foxmail.com>
2024-07-09 14:25:58 +0800

    uint8_t (*State)();

--- include/c-bsp-interface/IDigitalLed.h
+++ include/c-bsp-interface/IDigitalLed.h
@@ -7,4 +7,8 @@ typedef struct IDigitalLed
 	void (*TurnOn)();
 	void (*TurnOff)();
 	void (*Toggle)();
+
+	/// @brief LED 灯的状态。
+	/// @return 返回 true 表示 LED 灯当前为亮，返回 false 表示 LED 灯当前为灭。
+	uint8_t (*State)();
 } IDigitalLed;
