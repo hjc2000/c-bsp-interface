8c2ee8f32a547da697a3f89fb47957e185953a8b
hjc2000 <hjc.666@foxmail.com>
2024-07-10 15:54:40 +0800

    ModbusBufferReader

--- include/c-bsp-interface/modbus/ModbusBufferReader.c
+++ include/c-bsp-interface/modbus/ModbusBufferReader.c
@@ -1,8 +1,93 @@
 #include "ModbusBufferReader.h"
+#include <c-bsp-interface/memory/StackHeap.h>
 
 typedef struct ModbusBufferReader
 {
 	uint8_t *_buffer;
-	uint8_t *_position;
+	int32_t _position;
 	ModbusBitConverterUnit _unit;
 } ModbusBufferReader;
+
+ModbusBufferReader *ModbusBufferReader_StackHeapAlloc(ModbusBitConverterUnit unit)
+{
+	ModbusBufferReader *self = StackHeapObjAlloc(sizeof(ModbusBufferReader));
+	self->_buffer = 0;
+	self->_position = 0;
+	self->_unit = unit;
+	return self;
+}
+
+void ModbusBufferReader_SetBuffer(ModbusBufferReader *self, uint8_t *buffer)
+{
+	self->_buffer = buffer;
+}
+
+uint8_t ModbusBufferReader_ReadUInt8(ModbusBufferReader *self)
+{
+	uint8_t value = self->_buffer[self->_position];
+	self->_position++;
+	return value;
+}
+
+int8_t ModbusBufferReader_ReadInt8(ModbusBufferReader *self)
+{
+	int8_t value = self->_buffer[self->_position];
+	self->_position++;
+	return value;
+}
+
+uint16_t ModbusBufferReader_ReadUInt16(ModbusBufferReader *self)
+{
+	uint16_t value = ModbusBitConverter_ToUInt16(self->_unit, self->_buffer, self->_position);
+	self->_position += 2;
+	return value;
+}
+
+int16_t ModbusBufferReader_ReadInt16(ModbusBufferReader *self)
+{
+	int16_t value = ModbusBitConverter_ToInt16(self->_unit, self->_buffer, self->_position);
+	self->_position += 2;
+	return value;
+}
+
+uint32_t ModbusBufferReader_ReadUInt32(ModbusBufferReader *self)
+{
+	uint32_t value = ModbusBitConverter_ToUInt32(self->_unit, self->_buffer, self->_position);
+	self->_position += 4;
+	return value;
+}
+
+int32_t ModbusBufferReader_ReadInt32(ModbusBufferReader *self)
+{
+	int32_t value = ModbusBitConverter_ToInt32(self->_unit, self->_buffer, self->_position);
+	self->_position += 4;
+	return value;
+}
+
+uint64_t ModbusBufferReader_ReadUInt64(ModbusBufferReader *self)
+{
+	uint64_t value = ModbusBitConverter_ToUInt64(self->_unit, self->_buffer, self->_position);
+	self->_position += 8;
+	return value;
+}
+
+int64_t ModbusBufferReader_ReadInt64(ModbusBufferReader *self)
+{
+	int64_t value = ModbusBitConverter_ToUInt64(self->_unit, self->_buffer, self->_position);
+	self->_position += 8;
+	return value;
+}
+
+float ModbusBufferReader_ReadFloat(ModbusBufferReader *self)
+{
+	float value = ModbusBitConverter_ToFloat(self->_unit, self->_buffer, self->_position);
+	self->_position += 4;
+	return value;
+}
+
+double ModbusBufferReader_ReadDouble(ModbusBufferReader *self)
+{
+	double value = ModbusBitConverter_ToFloat(self->_unit, self->_buffer, self->_position);
+	self->_position += 8;
+	return value;
+}
--- include/c-bsp-interface/modbus/ModbusBufferReader.h
+++ include/c-bsp-interface/modbus/ModbusBufferReader.h
@@ -1,4 +1,33 @@
 #pragma once
 #include <c-bsp-interface/modbus/ModbusBitConverter.h>
 
+/// @brief 读取接收缓冲区。每次读取，内部记录的当前位置会自动递增。
 typedef struct ModbusBufferReader ModbusBufferReader;
+
+/// @brief 在栈堆上构造
+/// @warning 构造后需要使用 ModbusBufferReader_SetBuffer 设置一个缓冲区才能读取。
+/// 读取完 1 个缓冲区后需要再次调用 ModbusBufferReader_SetBuffer 设置一个新的缓冲区。
+///
+/// @param unit 转换单位
+/// @return
+ModbusBufferReader *ModbusBufferReader_StackHeapAlloc(ModbusBitConverterUnit unit);
+
+/// @brief 设置一个缓冲区。设置后才能读取，否则就会读取空指针或野指针指向的内存。
+/// @param self
+/// @param buffer
+void ModbusBufferReader_SetBuffer(ModbusBufferReader *self, uint8_t *buffer);
+
+uint8_t ModbusBufferReader_ReadUInt8(ModbusBufferReader *self);
+int8_t ModbusBufferReader_ReadInt8(ModbusBufferReader *self);
+
+uint16_t ModbusBufferReader_ReadUInt16(ModbusBufferReader *self);
+int16_t ModbusBufferReader_ReadInt16(ModbusBufferReader *self);
+
+uint32_t ModbusBufferReader_ReadUInt32(ModbusBufferReader *self);
+int32_t ModbusBufferReader_ReadInt32(ModbusBufferReader *self);
+
+uint64_t ModbusBufferReader_ReadUInt64(ModbusBufferReader *self);
+int64_t ModbusBufferReader_ReadInt64(ModbusBufferReader *self);
+
+float ModbusBufferReader_ReadFloat(ModbusBufferReader *self);
+double ModbusBufferReader_ReadDouble(ModbusBufferReader *self);
