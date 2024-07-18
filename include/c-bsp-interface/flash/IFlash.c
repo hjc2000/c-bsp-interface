05b97130f7c83135df43a517ea69a9b420adb52f
hjc2000 <hjc.666@foxmail.com>
2024-07-18 14:21:59 +0800

    typedef struct IFlash

--- include/c-bsp-interface/flash/IFlash.c
+++ include/c-bsp-interface/flash/IFlash.c
@@ -2,38 +2,38 @@
 
 size_t IFlash_GetAbsoluteAddress(IFlash *self, int32_t bank_id, size_t addr)
 {
-	if (addr >= self->GetBankSize(self->_self, bank_id))
+	if (addr >= self->GetBankSize(bank_id))
 	{
 		return 0;
 	}
 
-	return self->GetBankBaseAddress(self->_self, bank_id) + addr;
+	return self->GetBankBaseAddress(bank_id) + addr;
 }
 
 uint8_t IFlash_ReadUInt8(IFlash *self, int32_t bank_id, size_t addr)
 {
 	uint8_t ret;
-	self->ReadBufferAsync(self->_self, bank_id, addr, (uint8_t *)(&ret), sizeof(ret));
+	self->ReadBufferAsync(bank_id, addr, (uint8_t *)(&ret), sizeof(ret));
 	return ret;
 }
 
 uint16_t IFlash_ReadUInt16(IFlash *self, int32_t bank_id, size_t addr)
 {
 	uint16_t ret;
-	self->ReadBufferAsync(self->_self, bank_id, addr, (uint8_t *)(&ret), sizeof(ret));
+	self->ReadBufferAsync(bank_id, addr, (uint8_t *)(&ret), sizeof(ret));
 	return ret;
 }
 
 uint32_t IFlash_ReadUInt32(IFlash *self, int32_t bank_id, size_t addr)
 {
 	uint32_t ret;
-	self->ReadBufferAsync(self->_self, bank_id, addr, (uint8_t *)(&ret), sizeof(ret));
+	self->ReadBufferAsync(bank_id, addr, (uint8_t *)(&ret), sizeof(ret));
 	return ret;
 }
 
 uint64_t IFlash_ReadUInt64(IFlash *self, int32_t bank_id, size_t addr)
 {
 	uint64_t ret;
-	self->ReadBufferAsync(self->_self, bank_id, addr, (uint8_t *)(&ret), sizeof(ret));
+	self->ReadBufferAsync(bank_id, addr, (uint8_t *)(&ret), sizeof(ret));
 	return ret;
 }
--- include/c-bsp-interface/flash/IFlash.h
+++ include/c-bsp-interface/flash/IFlash.h
@@ -4,57 +4,47 @@
 
 typedef struct IFlash
 {
-	void *_self;
-
-	void (*Lock)(void *self);
-	void (*Unlock)(void *self);
+	void (*Lock)();
+	void (*Unlock)();
 
 	/// @brief 获取此 flash 的 bank 数量。
-	/// @param self
 	/// @return
-	int32_t (*BankCount)(void *self);
+	int32_t (*BankCount)();
 
 	/// @brief 获取指定 bank 的基地址。
-	/// @param self
 	/// @param bank_id bank 的 id。例如 bank1 的 id 是 1.
 	/// @return
-	size_t (*GetBankBaseAddress)(void *self, int32_t bank_id);
+	size_t (*GetBankBaseAddress)(int32_t bank_id);
 
 	/// @brief 获取指定 bank 的大小。单位：字节。
-	/// @param self
 	/// @param bank_id bank 的 id。例如 bank1 的 id 是 1.
 	/// @return
-	size_t (*GetBankSize)(void *self, int32_t bank_id);
+	size_t (*GetBankSize)(int32_t bank_id);
 
 	/// @brief flash 的最小编程单位。单位：字节。
-	/// @param self
 	/// @note 最小单位是一次编程必须写入这么多字节，即使要写入的数据没有这么多，在一次
 	/// 写入后，整个单位大小的区域都无法再次写入了，除非擦除整个扇区。
 	/// @return 返回此 flash 编程的最小单位。
-	int32_t (*MinProgrammingUnit)(void *self);
+	int32_t (*MinProgrammingUnit)();
 
 	/// @brief 擦除一整个 bank。
-	/// @param self
 	/// @param bank_id bank 的 id。例如 bank1 的 id 是 1.
-	void (*EraseBankAsync)(void *self, int32_t bank_id);
+	void (*EraseBankAsync)(int32_t bank_id);
 
 	/// @brief 擦除指定 bank 中从 start_sector_index 开始的 sector_count 个扇区。
-	/// @param self
 	/// @param bank_id bank 的 id。例如 bank1 的 id 是 1.
 	/// @param start_sector_index 要擦除的扇区的起始索引。
 	/// @param sector_count 要擦除的扇区的数量。
-	void (*EraseSectorAsync)(void *self, int32_t bank_id, int32_t start_sector_index, int32_t sector_count);
+	void (*EraseSectorAsync)(int32_t bank_id, int32_t start_sector_index, int32_t sector_count);
 
 	/// @brief 将 flash 的数据读取到缓冲区中
-	/// @param self
 	/// @param bank_id bank 的 id。例如 bank1 的 id 是 1.
 	/// @param addr
 	/// @param buffer
 	/// @param count
-	void (*ReadBufferAsync)(void *self, int32_t bank_id, size_t addr, uint8_t *buffer, int32_t count);
+	void (*ReadBufferAsync)(int32_t bank_id, size_t addr, uint8_t *buffer, int32_t count);
 
 	/// @brief 编程
-	/// @param self
 	/// @param bank_id bank 的 id。例如 bank1 的 id 是 1.
 	///
 	/// @param addr 要写入的数据相对于此 bank 的起始地址的地址。
@@ -66,7 +56,7 @@ typedef struct IFlash
 	/// @exception 不同平台对 buffer 有对齐要求。例如 stm32 的 HAL 要求 buffer 要 4 字节
 	/// 对齐。这里使用 uint8_t const * ，接口的实现者自己计算 buffer 能否被对齐字节数整除，
 	/// 不能整除抛出异常。
-	void (*ProgramAsync)(void *self, int32_t bank_id, size_t addr, uint8_t const *buffer);
+	void (*ProgramAsync)(int32_t bank_id, size_t addr, uint8_t const *buffer);
 
 	/// @brief 异步操作完成回调。
 	void (*AsyncOperationCompletedCallback)();
