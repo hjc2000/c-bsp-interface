14ed00e517d2bd27e2123c521f0731140ba6d6c8
hjc2000 <hjc.666@foxmail.com>
2025-04-28 10:40:47 +0800

    extern inline

--- include/c-bsp-interface/bsp_auto_bit_converter_t.h
+++ include/c-bsp-interface/bsp_auto_bit_converter_t.h
@@ -44,37 +44,98 @@ extern inline int16_t bsp_auto_bit_converter_t__to_int16(bsp_auto_bit_converter_
 	return ret;
 }
 
-uint32_t AutoBitConverter_ToUInt32(bsp_auto_bit_converter_t *self, uint8_t const *buffer, int32_t offset);
-int32_t AutoBitConverter_ToInt32(bsp_auto_bit_converter_t *self, uint8_t const *buffer, int32_t offset);
+extern inline uint32_t bsp_auto_bit_converter_t__to_uint32(
+	bsp_auto_bit_converter_t *self, uint8_t const *buffer, int32_t offset)
+{
+	uint32_t ret = BitConverter_ByteArrayToUInt32(buffer, offset);
+	if (bsp_auto_bit_converter_t__should_reverse(self))
+	{
+		ReverseByteArray((uint8_t *)(&ret), 4);
+	}
 
-uint64_t AutoBitConverter_ToUInt64(bsp_auto_bit_converter_t *self, uint8_t const *buffer, int32_t offset);
-int64_t AutoBitConverter_ToInt64(bsp_auto_bit_converter_t *self, uint8_t const *buffer, int32_t offset);
+	return ret;
+}
 
-float AutoBitConverter_ToFloat(bsp_auto_bit_converter_t *self, uint8_t const *buffer, int32_t offset);
-double AutoBitConverter_ToDouble(bsp_auto_bit_converter_t *self, uint8_t const *buffer, int32_t offset);
+extern inline int32_t bsp_auto_bit_converter_t__to_int32(
+	bsp_auto_bit_converter_t *self, uint8_t const *buffer, int32_t offset)
+{
+	int32_t ret = BitConverter_ByteArrayToInt32(buffer, offset);
+	if (bsp_auto_bit_converter_t__should_reverse(self))
+	{
+		ReverseByteArray((uint8_t *)(&ret), 4);
+	}
+
+	return ret;
+}
+
+extern inline uint64_t AutoBitConverter_ToUInt64(bsp_auto_bit_converter_t *self, uint8_t const *buffer, int32_t offset)
+{
+	uint64_t ret = BitConverter_ByteArrayToUInt64(buffer, offset);
+	if (bsp_auto_bit_converter_t__should_reverse(self))
+	{
+		ReverseByteArray((uint8_t *)(&ret), 8);
+	}
+
+	return ret;
+}
+
+extern inline int64_t AutoBitConverter_ToInt64(bsp_auto_bit_converter_t *self, uint8_t const *buffer, int32_t offset)
+{
+	int64_t ret = BitConverter_ByteArrayToInt64(buffer, offset);
+	if (bsp_auto_bit_converter_t__should_reverse(self))
+	{
+		ReverseByteArray((uint8_t *)(&ret), 8);
+	}
+
+	return ret;
+}
+
+extern inline float AutoBitConverter_ToFloat(bsp_auto_bit_converter_t *self, uint8_t const *buffer, int32_t offset)
+{
+	float ret = BitConverter_ByteArrayToFloat(buffer, offset);
+	if (bsp_auto_bit_converter_t__should_reverse(self))
+	{
+		ReverseByteArray((uint8_t *)(&ret), 4);
+	}
+
+	return ret;
+}
+
+extern inline double AutoBitConverter_ToDouble(bsp_auto_bit_converter_t *self, uint8_t const *buffer, int32_t offset)
+{
+	double ret = BitConverter_ByteArrayToDouble(buffer, offset);
+	if (bsp_auto_bit_converter_t__should_reverse(self))
+	{
+		ReverseByteArray((uint8_t *)(&ret), 8);
+	}
+
+	return ret;
+}
 
 /* #endregion */
 
 /* #region 转字节数组 */
 
-void AutoBitConverter_GetBytesFromUInt16(bsp_auto_bit_converter_t *self,
-										 uint16_t value, uint8_t *out_buffer, int32_t offset);
-void AutoBitConverter_GetBytesFromInt16(bsp_auto_bit_converter_t *self,
-										int16_t value, uint8_t *out_buffer, int32_t offset);
+extern inline void AutoBitConverter_GetBytesFromUInt16(bsp_auto_bit_converter_t *self,
+													   uint16_t value, uint8_t *out_buffer, int32_t offset);
 
-void AutoBitConverter_GetBytesFromUInt32(bsp_auto_bit_converter_t *self,
-										 uint32_t value, uint8_t *out_buffer, int32_t offset);
-void AutoBitConverter_GetBytesFromInt32(bsp_auto_bit_converter_t *self,
-										int32_t value, uint8_t *out_buffer, int32_t offset);
+extern inline void AutoBitConverter_GetBytesFromInt16(bsp_auto_bit_converter_t *self,
+													  int16_t value, uint8_t *out_buffer, int32_t offset);
 
-void AutoBitConverter_GetBytesFromUInt64(bsp_auto_bit_converter_t *self,
-										 uint64_t value, uint8_t *out_buffer, int32_t offset);
-void AutoBitConverter_GetBytesFromInt64(bsp_auto_bit_converter_t *self,
-										int64_t value, uint8_t *out_buffer, int32_t offset);
+extern inline void AutoBitConverter_GetBytesFromUInt32(bsp_auto_bit_converter_t *self,
+													   uint32_t value, uint8_t *out_buffer, int32_t offset);
+extern inline void AutoBitConverter_GetBytesFromInt32(bsp_auto_bit_converter_t *self,
+													  int32_t value, uint8_t *out_buffer, int32_t offset);
 
-void AutoBitConverter_GetBytesFromFloat(bsp_auto_bit_converter_t *self,
-										float value, uint8_t *out_buffer, int32_t offset);
-void AutoBitConverter_GetBytesFromDouble(bsp_auto_bit_converter_t *self,
-										 double value, uint8_t *out_buffer, int32_t offset);
+extern inline void AutoBitConverter_GetBytesFromUInt64(bsp_auto_bit_converter_t *self,
+													   uint64_t value, uint8_t *out_buffer, int32_t offset);
+extern inline void AutoBitConverter_GetBytesFromInt64(bsp_auto_bit_converter_t *self,
+													  int64_t value, uint8_t *out_buffer, int32_t offset);
+
+extern inline void AutoBitConverter_GetBytesFromFloat(bsp_auto_bit_converter_t *self,
+													  float value, uint8_t *out_buffer, int32_t offset);
+
+extern inline void AutoBitConverter_GetBytesFromDouble(bsp_auto_bit_converter_t *self,
+													   double value, uint8_t *out_buffer, int32_t offset);
 
 /* #endregion */
