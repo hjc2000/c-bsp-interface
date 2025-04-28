cbb926f91edd1709fb24fb279cdbb5fdf9fa0703
hjc2000 <hjc.666@foxmail.com>
2025-04-28 11:22:14 +0800

    inline

--- include/c-bsp-interface/BitConverter.c
+++ include/c-bsp-interface/BitConverter.c
@@ -1,46 +1,6 @@
 #include "BitConverter.h"
 #include <string.h>
 
-#pragma region 转数字
-
-uint32_t BitConverter_4_Byte_To_UInt32(uint8_t b3, uint8_t b2, uint8_t b1, uint8_t b0)
-{
-	uint16_t high = BitConverter_2_Byte_To_UInt16(b3, b2);
-	uint16_t low = BitConverter_2_Byte_To_UInt16(b1, b0);
-	return BitConverter_2_Uint16_To_UInt32(high, low);
-}
-
-uint32_t BitConverter_2_Uint16_To_UInt32(uint16_t high, uint16_t low)
-{
-	return ((uint32_t)high << 16) | low;
-}
-
-uint64_t BitConverter_ByteArrayToUInt64(uint8_t const *buffer, int32_t offset)
-{
-	uint64_t const *p = (uint64_t const *)(buffer + offset);
-	return *p;
-}
-
-int64_t BitConverter_ByteArrayToInt64(uint8_t const *buffer, int32_t offset)
-{
-	int64_t const *p = (int64_t const *)(buffer + offset);
-	return *p;
-}
-
-float BitConverter_ByteArrayToFloat(uint8_t const *buffer, int32_t offset)
-{
-	float const *p = (float const *)(buffer + offset);
-	return *p;
-}
-
-double BitConverter_ByteArrayToDouble(uint8_t const *buffer, int32_t offset)
-{
-	double const *p = (double const *)(buffer + offset);
-	return *p;
-}
-
-#pragma endregion
-
 #pragma region 转字节数组
 
 void BitConverter_GetBytesFromUInt16(uint16_t value, uint8_t *out_buffer, int32_t offset)
--- include/c-bsp-interface/BitConverter.h
+++ include/c-bsp-interface/BitConverter.h
@@ -32,14 +32,41 @@ inline int32_t BitConverter_ByteArrayToInt32(uint8_t const *buffer, int32_t offs
 	return *p;
 }
 
-uint32_t BitConverter_4_Byte_To_UInt32(uint8_t b3, uint8_t b2, uint8_t b1, uint8_t b0);
-uint32_t BitConverter_2_Uint16_To_UInt32(uint16_t high, uint16_t low);
+inline uint32_t BitConverter_2_Uint16_To_UInt32(uint16_t high, uint16_t low)
+{
+	return ((uint32_t)high << 16) | low;
+}
 
-uint64_t BitConverter_ByteArrayToUInt64(uint8_t const *buffer, int32_t offset);
-int64_t BitConverter_ByteArrayToInt64(uint8_t const *buffer, int32_t offset);
+inline uint32_t BitConverter_4_Byte_To_UInt32(uint8_t b3, uint8_t b2, uint8_t b1, uint8_t b0)
+{
+	uint16_t high = BitConverter_2_Byte_To_UInt16(b3, b2);
+	uint16_t low = BitConverter_2_Byte_To_UInt16(b1, b0);
+	return BitConverter_2_Uint16_To_UInt32(high, low);
+}
 
-float BitConverter_ByteArrayToFloat(uint8_t const *buffer, int32_t offset);
-double BitConverter_ByteArrayToDouble(uint8_t const *buffer, int32_t offset);
+inline uint64_t BitConverter_ByteArrayToUInt64(uint8_t const *buffer, int32_t offset)
+{
+	uint64_t const *p = (uint64_t const *)(buffer + offset);
+	return *p;
+}
+
+inline int64_t BitConverter_ByteArrayToInt64(uint8_t const *buffer, int32_t offset)
+{
+	int64_t const *p = (int64_t const *)(buffer + offset);
+	return *p;
+}
+
+inline float BitConverter_ByteArrayToFloat(uint8_t const *buffer, int32_t offset)
+{
+	float const *p = (float const *)(buffer + offset);
+	return *p;
+}
+
+inline double BitConverter_ByteArrayToDouble(uint8_t const *buffer, int32_t offset)
+{
+	double const *p = (double const *)(buffer + offset);
+	return *p;
+}
 
 /* #endregion */
 
