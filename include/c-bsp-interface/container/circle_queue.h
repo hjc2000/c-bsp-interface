998107b10be6c6b488f54922ad872d375de2912d
hjc2000 <hjc.666@foxmail.com>
2025-05-13 16:38:16 +0800

    bsp_circle_queue

--- include/c-bsp-interface/container/circle_queue.h
+++ include/c-bsp-interface/container/circle_queue.h
@@ -8,12 +8,23 @@
 typedef struct bsp_circle_queue
 {
 	///
+	/// @brief 总大小。
+	///
+	/// @note 预先分配一个字节数组，将数组头指针强制转换为本结构体的指针，然后初始化，
+	/// 相当于 C++ 的 placement new. 本字段用来记录这个字节数组的总大小。
+	///
+	///
+	size_t _total_size;
+
+	///
 	/// @brief 元素个数。
 	///
 	///
 	size_t _count;
+
 	size_t _head;
 	size_t _tail;
+
 	__element_type _array[];
 
 } bsp_circle_queue;
