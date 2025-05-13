412eaf1e79d1a5cba78b74afbc2a5db384428259
hjc2000 <hjc.666@foxmail.com>
2025-05-13 17:18:22 +0800

    bsp_circle_queue_required_uint64_array_count

--- include/c-bsp-interface/container/circle_queue.h
+++ include/c-bsp-interface/container/circle_queue.h
@@ -30,10 +30,25 @@ typedef struct bsp_circle_queue
 } bsp_circle_queue;
 
 ///
+/// @brief 想要存放 N 个元素，需要准备多少个字节的内存块。
+///
+///
+#define bsp_circle_queue_required_memory_block_size(N) (sizeof(bsp_circle_queue) + N * sizeof(__element_type))
+
+///
+/// @brief 想要存放 N 个元素，需要准备具有多少个元素的 uint64_t 数组。
+///
+///
+#define bsp_circle_queue_required_uint64_array_count(N) (bsp_circle_queue_required_memory_block_size(N) / 8 + 1)
+
+///
 /// @brief 在 memory_block 上构造 bsp_circle_queue.
 ///
 /// @param memory_block uint64_t 的数组。
 /// 	@note 使用 uint64_t 数组是为了保证内存对齐。
+///
+/// 	@note 使用 bsp_circle_queue_required_uint64_array_count 来计算需要一个具有
+/// 	多少个元素的 uint64_t 数组。
 ///
 /// @param total_size memory_block 的总字节数。
 ///
