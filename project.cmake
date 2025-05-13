f5f09b30ed8107ce142c18bfd9c92307570dc8a5
hjc2000 <hjc.666@foxmail.com>
2025-05-13 16:24:22 +0800

    添加测试程序

--- project.cmake
+++ project.cmake
@@ -1,3 +1,11 @@
 # 编译目标
 add_library(${ProjectName})
 target_import_src(${ProjectName})
+
+# 添加测试程序
+if(platform STREQUAL "msys")
+	set(exe_name "test")
+	add_executable(${exe_name} ${CMAKE_CURRENT_SOURCE_DIR}/exe/main.c)
+	target_link_libraries(${exe_name} ${ProjectName})
+	target_install(${exe_name})
+endif()
