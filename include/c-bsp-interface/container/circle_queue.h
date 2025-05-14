2d332467de4802f57377b7cc0ff4c91689397fdb
hjc2000 <hjc.666@foxmail.com>
2025-05-14 15:45:25 +0800

    refactor(circle_queue): 重构环形队列实现并添加操作函数
    重构环形队列结构体字段命名，移除_total_size改为_max_count，并添加队列操作函数包括入队、出队、计数和判空等功能

--- include/c-bsp-interface/container/circle_queue.h
+++ include/c-bsp-interface/container/circle_queue.h
@@ -12,17 +12,9 @@
 
 typedef struct bsp_circle_queue
 {
-	///
-	/// @brief 总大小。
-	///
-	/// @note 预先分配一个字节数组，将数组头指针强制转换为本结构体的指针，然后初始化，
-	/// 相当于 C++ 的 placement new. 本字段用来记录这个字节数组的总大小。
-	///
-	///
-	size_t _total_size;
-
-	size_t _head;
-	size_t _tail;
+	size_t _max_count;
+	size_t _start;
+	size_t _end;
 	bsp_bool _is_full;
 
 	__element_type _array[];
@@ -41,6 +33,46 @@ typedef struct bsp_circle_queue
 ///
 #define bsp_circle_queue_required_uint64_array_count(N) (bsp_circle_queue_required_memory_block_size(N) / 8 + 1)
 
+/* #region 私有 */
+
+static void bsp_circle_queue_add_start(bsp_circle_queue *self)
+{
+	self->_start++;
+	self->_start %= self->_max_count;
+}
+
+static void bsp_circle_queue_sub_start(bsp_circle_queue *self)
+{
+	if (self->_start == 0)
+	{
+		self->_start = self->_max_count - 1;
+	}
+	else
+	{
+		self->_start--;
+	}
+}
+
+static void bsp_circle_queue_add_end(bsp_circle_queue *self)
+{
+	self->_end++;
+	self->_end %= self->_max_count;
+}
+
+static void bsp_circle_queue_sub_end(bsp_circle_queue *self)
+{
+	if (self->_end == 0)
+	{
+		self->_end = self->_max_count - 1;
+	}
+	else
+	{
+		self->_end--;
+	}
+}
+
+/* #endregion */
+
 ///
 /// @brief 在 memory_block 上构造 bsp_circle_queue.
 ///
@@ -50,18 +82,71 @@ typedef struct bsp_circle_queue
 /// 	@note 使用 bsp_circle_queue_required_uint64_array_count 来计算需要一个具有
 /// 	多少个元素的 uint64_t 数组。
 ///
-/// @param total_size memory_block 的总字节数。
+/// @param max_count 最大可以存放的元素个数。
 ///
 /// @return
 ///
-static bsp_circle_queue *bsp_circle_queue_placement_new(uint64_t *memory_block, size_t total_size)
+static bsp_circle_queue *bsp_circle_queue_placement_new(uint64_t *memory_block, size_t max_count)
 {
 	bsp_circle_queue *ret = (bsp_circle_queue *)(memory_block);
-	ret->_total_size = total_size;
-	ret->_head = 0;
-	ret->_tail = 0;
+	ret->_max_count = max_count;
+	ret->_start = 0;
+	ret->_end = 0;
 	ret->_is_full = 0;
 	return ret;
+}
+
+static bsp_bool bsp_circle_queue_is_empty(bsp_circle_queue *self)
+{
+	if (self->_start == self->_end && !self->_is_full)
+	{
+		return bsp_true;
+	}
+
+	return bsp_false;
+}
+
+static size_t bsp_circle_queue_count(bsp_circle_queue *self)
+{
+	if (self->_is_full)
+	{
+		return self->_max_count;
+	}
+
+	if (self->_end < self->_start)
+	{
+		// 发生了环绕。
+		return self->_max_count - (self->_start - self->_end);
+	}
+
+	return self->_end - self->_start;
+}
+
+static bsp_bool bsp_circle_queue_enqueue(bsp_circle_queue *self, __element_type const *element)
+{
+	if (self->_is_full)
+	{
+		return 0;
+	}
+
+	self->_array[self->_end] = *element;
+	bsp_circle_queue_add_end(self);
+	if (self->_start == self->_end)
+	{
+		self->_is_full = bsp_true;
+	}
+}
+
+static bsp_bool bsp_circle_queue_dequeue(bsp_circle_queue *self, __element_type *out)
+{
+	if (bsp_circle_queue_is_empty(self))
+	{
+		return bsp_false;
+	}
+
+	*out = self->_array[self->_start];
+	bsp_circle_queue_sub_start(self);
+	return bsp_true;
 }
 
 #undef __element_type
